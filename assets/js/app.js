588fc673ab6b8da46da437e2cfd1b0bde132aeda
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2022-03-16 15:26:30 +0100

    maj

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
 window.addEventListener('load', () => {
   document.body.classList.remove("clean__transition");
 })
+
+// Menu burger
+const btnBurger = document.querySelector('.burger__btn');
+let navBar = document.querySelector('.nav__container');
+let items = document.querySelectorAll('.a_nav')
+
+function burger() {
+  btnBurger.classList.toggle('active')
+  navBar.classList.toggle('active__menu');
+  items.forEach(element => {
+    element.classList.toggle('active');
+  });
+}
+btnBurger.addEventListener('click', burger);
